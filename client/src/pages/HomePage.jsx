048eebc03cdb43e4aa7e57871a50847727bde334
sabhay02 <sabhaythakkar02@gmail.com>
2025-07-09 23:14:08 +0530

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,26 +5,17 @@ import RightSIdebar from '../components/RightSIdebar'
 import { ChatContext } from '../context/ChatContext'
 
 const HomePage = () => {
-  const { selectedUser } = useContext(ChatContext)
+const {selectedUser}=useContext(ChatContext)
 
-  return (
+return (
     <div className='w-full h-screen px-2 py-2 sm:px-4 sm:py-4 lg:px-8 lg:py-6 xl:px-[10%] xl:py-[4%]'>
-      <div className={`
-        glass-effect rounded-3xl overflow-hidden h-full grid relative
-        shadow-2xl border border-white/20
-        ${selectedUser 
-          ? 'grid-cols-1 lg:grid-cols-[300px_1fr_320px] xl:grid-cols-[340px_1fr_360px]' 
-          : 'grid-cols-1 lg:grid-cols-[300px_1fr] xl:grid-cols-[340px_1fr]'
-        }
-      `}>
-        <Sidebar />
-        <ChatContianer />
-        {selectedUser && <RightSIdebar />}
-      </div>
+        <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid relative ${selectedUser ? 'grid-cols-1 lg:grid-cols-[280px_1fr_300px] xl:grid-cols-[320px_1fr_350px]' : 'grid-cols-1 lg:grid-cols-[280px_1fr] xl:grid-cols-[320px_1fr]'}`}>
+            <Sidebar />
+            <ChatContianer />
+            {selectedUser && <RightSIdebar />}
+        </div>
     </div>
-  )
+)
 }
 
-
-
-export default HomePage;
+export default HomePage
